6c5a44b4069ad0ec1885c476e56d0c8c3840d3a9
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-09-26 18:23:43 -0300

    Create models.py
    criação do models de usuario(utilizando o padrão do django) é o models de task

---
+++ gerenciamento_de_tarefa/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User #criando usuarios utilizando o usuario padrão do django
+
+class task(models.Model):
+    Titulo = models.CharField(max_length=100)
+    Descrição = models.TextField()
+    data_criação = models.DateTimeField()
+    Data_limite = models.DateTimeField()
+    status = models.BooleanField()
+    atribuida_a = models.ForeignKey(User, on_delete=models.CASCADE )
