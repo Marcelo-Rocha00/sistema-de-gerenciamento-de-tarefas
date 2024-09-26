c9fa9d5df1348cf127b712203b5b22b423a17c5a
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-09-26 18:49:13 -0300

    criação de serializer e viewsets
    criei o serializer e as views para montar a Api

--- gerenciamento_de_tarefa/views.py
+++ gerenciamento_de_tarefa/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import task
+from .serializers import taskSerializer
 
-# Create your views here.
+class taskViewSet(viewsets.ModelViewSet):
+    class Meta:
+        queryset = task.objects.all()
+        serializer_class = taskSerializer
+
