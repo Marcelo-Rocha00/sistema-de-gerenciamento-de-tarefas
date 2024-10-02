5df444ccaa766da2d2d521ba6231c2247b972630
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-10-02 18:37:25 -0300

    att
    criação das views é das rotas urls

--- gerenciamento_de_tarefa/url.py
+++ gerenciamento_de_tarefa/url.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import taskViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'task', taskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    
+    path('registro/', views.SignUP.as_view(), name="SignUP")
 ] 
 
 
--- gerenciamento_de_tarefa/views.py
+++ gerenciamento_de_tarefa/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .models import task
 from .serializers import taskSerializer
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import CustomUserCreationForm #importando o formulario personalizado com o e-mail
 
 class taskViewSet(viewsets.ModelViewSet):
     queryset = task.objects.all()
     serializer_class = taskSerializer
         
-        
+class SignUP(generic.CreateView):#utilizando o 'generic' que ajuda na criação de views
+    form_class = CustomUserCreationForm #usando o formulario personalizado
+    success_url = reverse_lazy('registro') # URL de redirecionamento após registro bem-sucedido
+    template_name = 'registro.html' ## Nome do template HTML a ser renderizado para o registro
         
         
 
