a1c0b2a2c4fbeac215bace23891966cb32557bb8
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-10-10 18:21:45 -0300

    att
    adicionando comentarios

--- gerenciamento_de_tarefa/models.py
+++ gerenciamento_de_tarefa/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User #criando usuarios utilizando o usuario padrão do django
 
-class task(models.Model):
-    Titulo = models.CharField(max_length=100)
-    Descrição = models.TextField()
-    data_criação = models.DateTimeField()
-    Data_limite = models.DateTimeField()
-    status = models.BooleanField()
-    atribuida_a = models.ForeignKey(User, on_delete=models.CASCADE )
+class task(models.Model): #definindo um modelo pra task
+    Titulo = models.CharField(max_length=100) # campo onde o titulo sera inserido, limitado a 100 caracteres
+    Descrição = models.TextField()# campo onde é possivel inserir uma descrição detalhada do pedido
+    data_criação = models.DateTimeField()# data é hora da criação do pedido
+    Data_limite = models.DateTimeField()# data é hora limite para a conclusão da tarefa
+    status = models.BooleanField()# Status da tarefa: True se concluida é False caso não esteja concluida ainda
+    atribuida_a = models.ForeignKey(User, on_delete=models.CASCADE ) # usuário ao qual a tarefa esta atribuida
     
     def __str__(self):
-        return self.Titulo
+        return self.Titulo #retornando o título da tarefa como representação da instância
--- gerenciamento_de_tarefa/serializers.py
+++ gerenciamento_de_tarefa/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from .models import task
 
-class taskSerializer(serializers.ModelSerializer):
+class taskSerializer(serializers.ModelSerializer): #criando um serializers para o modelo task
     class Meta:
-        model = task
-        fields = "__all__"
+        model = task #especificando a qual model pertence esse serializer
+        fields = "__all__" #incluindo todos os campos do model para o serializer
         
 
