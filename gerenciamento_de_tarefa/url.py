cda0410b2cf307afd427692b8a64ef1d900cdf59
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-10-13 00:36:42 -0300

    Update url.py
    configuração das url

--- gerenciamento_de_tarefa/url.py
+++ gerenciamento_de_tarefa/url.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.contrib.auth.views import LogoutView
 from .views import taskViewSet
 from . import views
 
@@ -8,8 +9,13 @@ router.register(r'task', taskViewSet) # Registra o viewset de tarefas na rota 't
 
 urlpatterns = [
     path('api/', include(router.urls)), #uma rota url para acesso da API
-    path('registro/', views.SignUP.as_view(), name="registro"),  #Rota de acesso a pagina de registro
-    path('Login/', views.Login.as_view(), name = "login")
+    path('registro/', views.SignUP.as_view(), name='registro'),  #Rota de acesso a pagina de registro
+    path('login/', views.Login.as_view(), name = 'login'),
+    path('usuario/', views.perfil_usuario, name = 'usuario' ),
+    path('logout/',LogoutView.as_view(next_page='login'), name= 'logout'),
+   
 ] 
+
+
 
 
