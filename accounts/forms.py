76952429d4d1114cb520724ddeb57cc872798543
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-11-05 20:10:46 -0300

    att

---
+++ accounts/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm): #criando um formulario personalizado para o modelo padrão User
+    #adicionando um campo de e-mail ao fromulario
+    email = forms.EmailField(required=True) #definindo que o email é obrigatorio
+    
+    class Meta:
+        model = User #utilizando o modelo usuario
+        fields = ['username', 'email', 'password1', 'password2']#incluindo todos os campos desejados
+       
+    #criando um metodo que salva o usuário. o 'commit' indica que o usuário dever ser salvo no banco de dados 
+    def save(self, commit=True): 
+        
+         #O método 'super()' chama o método 'save' da classe pai (UserCreationForm)
+        #O 'commit=False' é utilizado para evitar que o usuário seja salvo no banco de dados imediatamente.
+        #permitindo que eu faça modificações nos dados do usuário(que nesse codigo é atribuir o e-mail) antes de salvar
+        user = super().save(commit=False) 
+        
+        #atribuindo o e-mail fornecido pelo formulário ao campo 'e-mail' do usuário.
+        user.email = self.cleaned_data['email'] #'cleaned_data' contém os dados validados do formulário
+        
+        if commit:
+            user.save()#finalmente salvando o usuario no banco de dados # Retornamos a instância do usuário, que pode ser utilizada após o salvamento
+        return user
--- accounts/urls.py
+++ accounts/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
     path('login/redefinir/', auth_views.PasswordResetView.as_view(template_name ='Register/password_reset_form.html'), name='senha_redifinir'),
 
     #URL que sera exibido quando o e-mail de recuperação for enviado
-    path('login/redefinir/enviado/', auth_views.PasswordResetDoneView.as_view(template_name = 'Register/password_reset_done.html'), name="password_reset_done"),
+    path('login/redefinir/enviado/', auth_views.PasswordResetDoneView.as_view(template_name = 'Register/password_reset_done.html'), name="password_reset_enviada"),
 
     #URL que confirma a redifinição da senha(após o usuário clicar no link do e-mail)
-    path('login/redefinir/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='Register/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('login/redefinir/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='Register/password_reset_confirm.html'), name='password_reset_confirmada'),
 
     #URL que será exibido após a conclusão da redifinição da senha
-    path('login/redefinir/concluido/', auth_views.PasswordResetCompleteView.as_view(template_name='Register/password_reset_complete.html'), name='password_reset_complete'),
+    path('login/redefinir/concluido/', auth_views.PasswordResetCompleteView.as_view(template_name='Register/password_reset_complete.html'), name='password_reset_completa'),
     
 ]
