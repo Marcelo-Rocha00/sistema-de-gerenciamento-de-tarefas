067071ac46949deccd9d7a253b7a89a61656c79f
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-11-14 17:16:50 -0300

    criando um diretorio templates
    criando um diretorio templates fora dos apps para guardar os htmls de todos os apps em um so lugar

--- Project/settings.py
+++ Project/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -144,7 +144,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configurações de e-mail para enviar e-mails reais
 
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Define o backend como SMTP
 EMAIL_HOST = 'smtp.gmail.com'  # Exemplo: usando o Gmail
 EMAIL_PORT = 587  # Porta para TLS
@@ -152,3 +151,4 @@ EMAIL_USE_TLS = True  # Habilitar TLS
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') # Recupera o nome de usuário do e-mail a partir das variáveis de ambiente
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')  # Recupera a senha do e-mail a partir das variáveis de ambiente
 
+#configuração para autenticação via token
